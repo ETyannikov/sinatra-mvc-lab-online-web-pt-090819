ca6ab2991739779624527aafcadeddc21624c5d9
Eddy Tyannikov <etyannikov@gmail.com>
2020-01-14 01:46:57 +0000

    Automatically backed up by Learn

---
+++ models/piglatinizer.rb
@@ -0,0 +1,16 @@
+class PigLatinizer
+  attr_reader :text
+  
+  def initialize(text)
+    @text = text
+  end
+  
+  
+  def pig_latinize
+    #split sentence and do each word
+  end
+  
+  def word
+    #pig_latinize the word
+  end
+end
