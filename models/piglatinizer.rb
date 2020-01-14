57ef90ac6bd519bb100564ea433abc848743c1d1
Eddy Tyannikov <etyannikov@gmail.com>
2020-01-14 02:09:56 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,16 +1,37 @@
 class PigLatinizer
-  attr_reader :text
-  
-  def initialize(text)
-    @text = text
+
+  def piglatinize(input_str)
+    input_str.split(" ").length == 1 ? piglatinize_word(input_str) : piglatinize_sentence(input_str)
   end
-  
-  
-  def pig-latinize
-    @text.split(" ").length == 1 ? pig_word(@text) : @text.each {|text|text.pig_word}
+
+  private
+
+  def consonant?(char)
+    !char.match(/[aAeEiIoOuU]/)
   end
-  
-  def pig_word()
-    #pig_latinize the word
+
+  def piglatinize_word(word)
+    # word starts with vowel
+    if !consonant?(word[0])
+      word = word + "w"
+    # word starts with 3 consonants
+    elsif consonant?(word[0]) && consonant?(word[1]) && consonant?(word[2])
+      word = word.slice(3..-1) + word.slice(0,3)
+    # word starts with 2 consonants
+    elsif consonant?(word[0]) && consonant?(word[1])
+      word = word.slice(2..-1) + word.slice(0,2)
+    # word starts with 1 consonant
+    else
+      word = word.slice(1..-1) + word.slice(0)
+    end
+    word << "ay"
   end
+
+  def piglatinize_sentence(sentence)
+    sentence.split.collect { |word| piglatinize_word(word) }.join(" ")
+  end
+
+
+
+
 end
